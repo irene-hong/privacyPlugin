e479c660fb528205e8a2095a1dcd5e4aca94ca50
Jeremy Yu <90216803+Jer3myYu@users.noreply.github.com>
2023-02-21 03:35:21 -0500

    fix: add stderr

--- src/main/java/com/cmu/privacyplugin/PopupDialogAction.java
+++ src/main/java/com/cmu/privacyplugin/PopupDialogAction.java
@@ -2,12 +2,7 @@ package com.cmu.privacyplugin;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.ui.Messages;
 import org.jetbrains.annotations.NotNull;
-
-import java.io.BufferedReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class PopupDialogAction extends AnAction {
     public PopupDialogAction() {
@@ -19,8 +14,9 @@ public class PopupDialogAction extends AnAction {
         // Using the event, implement an action.
         // For example, create and show a dialog.
         TerminalCommand terminal = new TerminalCommand();
-        BufferedReader outputReader = terminal.runCommand(new String[]{"cmd", "/c", "dir"});
-        terminal.printTerminal(outputReader);
+        TerminalCommand.StandardIo outputReader = terminal.runCommand(new String[]{"cmd", "/c", "dir"});
+        terminal.printTerminal(outputReader.stdout);
+        terminal.printTerminal(outputReader.stderr);
     }
 
     // Override getActionUpdateThread() when you target 2022.3 or later!
--- src/main/java/com/cmu/privacyplugin/TerminalCommand.java
+++ src/main/java/com/cmu/privacyplugin/TerminalCommand.java
@@ -1,11 +1,22 @@
 package com.cmu.privacyplugin;
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
+
 public class TerminalCommand {
+    public class StandardIo {
+        BufferedReader stdout, stderr;
+
+        public StandardIo(InputStream stdoutStream, InputStream stderrStream) {
+            this.stdout = new BufferedReader(new InputStreamReader(stdoutStream));
+            this.stderr = new BufferedReader(new InputStreamReader(stderrStream));
+        }
+    }
+
     public TerminalCommand() {}
 
-    public BufferedReader runCommand(String[] cmdArray) {
+    public StandardIo runCommand(String[] cmdArray) {
         try {
             // Create the process builder and set the command
             ProcessBuilder processBuilder = new ProcessBuilder();
@@ -15,11 +26,12 @@ public class TerminalCommand {
             Process process = processBuilder.start();
 
             // Get the output of the command
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            StandardIo standardIo = new StandardIo(process.getInputStream(), process.getErrorStream());
+
             // Wait for the command to complete
             int exitCode = process.waitFor();
             System.out.println("Command exited with code " + exitCode);
-            return reader;
+            return standardIo;
         } catch (Exception e) {
             e.printStackTrace();
         }
